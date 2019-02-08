b6b42f08dfe33490ac2748d4623c6948c321640c
Justin Ndwiga <ndwigajustin@gmail.com>
2019-02-08 13:03:24 +0300

    add project boilerplate

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router-dom'; 
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import AboutMe from './components/aboutme';
+import Contact from './components/contact';
+import Projects from './components/projects';
+import Resume from './components/resume';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <div className="demo-big-content">
+      <Layout fixedHeader>
+          <Header title="Title" scroll>
+              <Navigation>
+                  <Link to="/resume">Resume</Link>
+                  <Link to="/aboutme">About Me</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/contact">Contact</Link>
+              </Navigation>  
+          </Header>
+          <Drawer title="Title">
+              <Navigation>
+                  <Link to="/resume">Resume</Link>
+                  <Link to="/aboutme">About Me</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/contact">Contact</Link>
+              </Navigation>
+          </Drawer>
+          <Content >
+            <div className="page-content" />
+            <Main />
+          </Content>
+      </Layout>
+  </div>
     );
   }
 }
 
 export default App;
+ 
