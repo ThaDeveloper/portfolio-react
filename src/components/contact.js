83f7b653ec3524300e9716099cd4406cab4033cd
Justin Ndwiga <ndwigajustin@gmail.com>
2019-02-10 22:28:20 +0300

    add contat page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react'
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 export default class Contact extends Component {
   render() {
     return (
-      <div>
-        <h1>Contact</h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Justin Ndwiga</h2>
+            <img 
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+              style={{height: '250px'}}
+            />
+            <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>Gangsta ipsizzle dolor sizzle amet, consectetuer adipiscing doggy. Nullizzle shit yo mamma, pot volutpizzle, suscipit its fo rizzle, its fo rizzle vizzle, arcu. Bizzle mah nizzle tortizzle. Sed erizzle</p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+            <div className="contact-list">
+            <List>
+              <ListItem>
+                <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton'}}>
+                <i className="fa fa-phone-square" aria-hidden="true" />
+                (254) 000000000
+                </ListItemContent>
+              </ListItem>
+              <ListItem>
+                <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton'}}>
+                <i className="fa fa-envelope" aria-hidden="true" />
+                myemail@gmail.com
+                </ListItemContent>
+              </ListItem>
+              <ListItem>
+                <ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton'}}>
+                <i className="fab fa-skype" aria-hidden="true" />
+                Skype ID
+                </ListItemContent>
+              </ListItem>
+            </List>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     )
   }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,201 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 export default class Projects extends Component {
+  state = {
+    activeTab: 0,
+  }
+
+  toggleCategories = () => {
+    switch(this.state.activeTab)
+    {
+      case 0:
+        return(
+          <div className="projects-grid">
+           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn-images-1.medium.com/max/1200/1*Qc0XxYm-qAZL-7tjjlNfrg.png) center / cover'}}>
+            Ruby Project #1
+            </CardTitle>
+            <CardText>
+            Lorizzle gangsta dolizzle sit amizzle, fo shizzle adipiscing boofron.Nullam dawg velizzle, dizzle volutpat, get down get down quizzle ...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn-images-1.medium.com/max/1200/1*Qc0XxYm-qAZL-7tjjlNfrg.png) center / cover'}}>
+            Ruby Project #2
+            </CardTitle>
+            <CardText>
+            Lorizzle gangsta dolizzle sit amizzle, fo shizzle adipiscing boofron.Nullam dawg velizzle, dizzle volutpat, get down get down quizzle ...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card> 
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn-images-1.medium.com/max/1200/1*Qc0XxYm-qAZL-7tjjlNfrg.png) center / cover'}}>
+            Ruby Project #3
+            </CardTitle>
+            <CardText>
+            Lorizzle gangsta dolizzle sit amizzle, fo shizzle adipiscing boofron.Nullam dawg velizzle, dizzle volutpat, get down get down quizzle ...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card> 
+         </div>
+        )
+      case 1:
+        return(
+         <div className="projects-grid">
+           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.python.org/static/opengraph-icon-200x200.png) center / cover'}}>
+            Python Project #1
+            </CardTitle>
+            <CardText>
+            Lorizzle gangsta dolizzle sit amizzle, fo shizzle adipiscing boofron.Nullam dawg velizzle, dizzle volutpat, get down get down quizzle ...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.python.org/static/opengraph-icon-200x200.png) center / cover'}}>
+            Python Project #2
+            </CardTitle>
+            <CardText>
+            Lorizzle gangsta dolizzle sit amizzle, fo shizzle adipiscing boofron.Nullam dawg velizzle, dizzle volutpat, get down get down quizzle ...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card> 
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.python.org/static/opengraph-icon-200x200.png) center / cover'}}>
+            Python Project #3
+            </CardTitle>
+            <CardText>
+            Lorizzle gangsta dolizzle sit amizzle, fo shizzle adipiscing boofron.Nullam dawg velizzle, dizzle volutpat, get down get down quizzle ...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card> 
+         </div>
+        )
+      case 2:
+        return(
+          <div className="projects-grid">
+           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png) center / cover'}}>
+            Javascript Project #1
+            </CardTitle>
+            <CardText>
+            Lorizzle gangsta dolizzle sit amizzle, fo shizzle adipiscing boofron.Nullam dawg velizzle, dizzle volutpat, get down get down quizzle ...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png) center / cover'}}>
+            Javascript Project #2
+            </CardTitle>
+            <CardText>
+            Lorizzle gangsta dolizzle sit amizzle, fo shizzle adipiscing boofron.Nullam dawg velizzle, dizzle volutpat, get down get down quizzle ...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card> 
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png) center / cover'}}>
+            Javascript Project #3
+            </CardTitle>
+            <CardText>
+            Lorizzle gangsta dolizzle sit amizzle, fo shizzle adipiscing boofron.Nullam dawg velizzle, dizzle volutpat, get down get down quizzle ...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card> 
+         </div> 
+        )
+      default:
+        return (
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px'}}>
+              Not Found
+            </CardTitle>
+          </Card>
+        )
+    }
+  }
+ 
+
   render() {
     return (
-      <div>
-        <h1>Projects</h1>
+      <div className="category-tabs">
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
+          <Tab>Ruby </Tab>
+          <Tab>Python</Tab>
+          <Tab>Javascript</Tab>
+        </Tabs>
+          <Grid>
+            <Cell col={12}>
+              <div className="content">
+                {this.toggleCategories()}
+              </div>
+            </Cell>
+          </Grid>
       </div>
-    )
+    ) 
   }
 }
